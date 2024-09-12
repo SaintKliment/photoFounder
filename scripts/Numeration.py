648e8b6d8f0002a2875172102b3a380bd14d0f34
SaintKliment <164558433+SaintKliment@users.noreply.github.com>
2024-09-13 00:08:07 +0300

    modification numeration

--- scripts/Numeration.py
+++ scripts/Numeration.py
@@ -1,14 +1,14 @@
 import os
 
-def rename_files_in_directory(directory):
+def rename_files_in_directory(directory, start_index=1):
     # Получаем список всех файлов в папке
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     
     # Сортируем файлы по имени, чтобы они шли в определенном порядке
     files.sort()
     
-    # Переименовываем файлы в формате image_1, image_2, ..., image_N
-    for i, filename in enumerate(files, start=1):
+    # Переименовываем файлы в формате img_{start_index}, img_{start_index+1}, ..., img_{start_index+N}
+    for i, filename in enumerate(files, start=start_index):
         # Получаем расширение файла
         file_extension = os.path.splitext(filename)[1]
         
@@ -23,8 +23,9 @@ def rename_files_in_directory(directory):
         os.rename(old_path, new_path)
         print(f"Переименован: {filename} -> {new_name}")
 
-# Укажите путь к вашей папке
+# Укажите путь к вашей папке и начальный индекс
 folder_path = './filters_photos'
+start_index = 1  # Например, начинаем с 100
 
 # Запуск функции переименования
-rename_files_in_directory(folder_path)
+rename_files_in_directory(folder_path, start_index)
